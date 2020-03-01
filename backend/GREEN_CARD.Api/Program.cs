4d0c7872854a4e0504ac60326130b0ccc03dc76d
Jeremy-Laier <jeremylaier@outlook.com>
2020-02-29 19:04:54 -0600

    Jeremy (#9)
    * api works
    
    * 75/80
    
    * fixed script error

--- backend/GREEN_CARD.Api/Program.cs
+++ backend/GREEN_CARD.Api/Program.cs
@@ -5,7 +5,6 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
-using Microsoft.Scripting.Hosting;
 
 namespace GREEN_CARD.Api {
     public class Program {
