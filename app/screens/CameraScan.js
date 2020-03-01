eed8952fe27e291312e9f455af4179910304c15f
Zhongyi Chen <zhongyi753@gmail.com>
2020-03-01 07:58:15 -0600

    create history page

--- app/screens/CameraScan.js
+++ app/screens/CameraScan.js
@@ -111,7 +111,7 @@ export class ImageView extends React.Component {
   img = null;
 
   state = {
-    language: ''
+    id: ''
   }
 
   static navigationOptions = {
@@ -167,19 +167,20 @@ export class ImageView extends React.Component {
         <View style={{
           marginTop: 10,
           width: winWidth,
-          backgroundColor: 'red',
           height: 300,
           alignItems: 'center',
         }}>
-          <Text style={{ fontSize: 25 }}>Select Date: </Text>
+          <Text style={{ fontSize: 25 }}>Select TransactionId: </Text>
           <Picker
-            selectedValue={this.state.language}
+            selectedValue={this.state.id}
             style={{ top: -50, height: 20, width: '100%' }}
             onValueChange={(itemValue, itemIndex) =>
-              this.setState({ language: itemValue })
+              this.setState({ id: itemValue })
             }>
-            <Picker.Item label="Java" value="java" />
-            <Picker.Item label="JavaScript" value="js" />
+            <Picker.Item label="3" value="3" />
+            <Picker.Item label="2" value="2" />
+            <Picker.Item label="1" value="1" />
+            <Picker.Item label="0" value="0" />
           </Picker>
           <TouchableOpacity style={{
             backgroundColor: '#DDDDDD',
